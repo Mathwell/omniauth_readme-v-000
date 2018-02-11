5435e421b2e04e71a7d05d8c4c329cb6a11a1735
Olena Ageyeva <eageeva@hotmail.com>
2018-02-11 18:44:34 +0000

    automatically backed up by learn

--- db/migrate/20180211173302_create_users.rb
+++ db/migrate/20180211173302_create_users.rb
@@ -2,9 +2,9 @@ class CreateUsers < ActiveRecord::Migration
   def change
     create_table :users do |t|
       t.string   :name
-      t.string :email
-      t.string :image
-      t.integer  :uid
+      t.string   :email
+      t.string   :image
+      t.string  :uid
       t.timestamps null: false
     end
   end
