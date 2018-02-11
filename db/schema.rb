c31c4e7fbcf01f4d93ce33671d65168aedf54168
Olena Ageyeva <eageeva@hotmail.com>
2018-02-11 18:44:53 +0000

    automatically backed up by learn

--- db/schema.rb
+++ db/schema.rb
@@ -11,15 +11,6 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180211173302) do
-
-  create_table "users", force: :cascade do |t|
-    t.string   "name"
-    t.string   "email"
-    t.string   "image"
-    t.integer  "uid"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
+ActiveRecord::Schema.define(version: 0) do
 
 end
